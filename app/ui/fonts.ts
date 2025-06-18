5e995ba96c5440f498ff1f57e5e3cc88ec1d2c63
Federico <federicocirme@gmail.com>
2025-06-18 17:55:36 +0200

    First commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+// This file is used to import and configure Google Fonts for use in a Next.js application.
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const lusitana = Lusitana({weight: ['400', '700'], subsets: ['latin']});
